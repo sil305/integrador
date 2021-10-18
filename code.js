5c8deed0be8cb03e5b45d831ddb22a78db996f4a
sil305 <90868156+sil305@users.noreply.github.com>
2021-10-17 23:08:18 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+let $estudiante=document.querySelector('.estudiante');
+let $estudianteValor=Number($estudiante.getElementsByClassName('estvalor')[0].getAttribute('value'));
+
+let $trainee=document.querySelector('.trainee');
+let $traineeValor=Number($trainee.getElementsByClassName('travalor')[0].getAttribute('value'));
+
+let $junior=document.querySelector('.junior');
+let $juniorValor=Number($junior.getElementsByClassName('juvalor')[0].getAttribute('value'));
